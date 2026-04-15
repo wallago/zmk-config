7a68fb278530f3358c059c36c5db18b725b522f3
wallago <45556867+wallago@users.noreply.github.com>
2026-04-15 14:21:16 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -3,6 +3,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     project-banner.url = "github:wallago/project-banner?dir=nix";
+
+    # Zephyr sdk and toolchain
+    zephyr-nix = {
+      url = "github:urob/zephyr-nix";
+    };
   };
 
   outputs =
@@ -10,40 +15,43 @@
       nixpkgs,
       flake-utils,
       project-banner,
+      zephyr-nix,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        zephyr = zephyr-nix.packages.${system};
       in
       {
         devShells = {
           default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              python313Packages.west
-              python313Packages.pyelftools
-              python313Packages.setuptools
-              python313Packages.protobuf
-              cmake
-              ninja
-              protobuf
-            ];
+            buildInputs =
+              with pkgs;
+              [
+                cmake
+                ninja
+                dtc
+                python313Packages.setuptools
+                python313Packages.pyelftools
+                python313Packages.protobuf
+                protobuf
+              ]
+              ++ [
+                zephyr.sdk
+                zephyr.pythonEnv
+              ];
             shellHook = ''
-              export ZEPHYR_BASE=$PWD/zephyr
               export CMAKE_PREFIX_PATH=$PWD/zephyr/share/zephyr-package/cmake
               export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb
-              export GNUARMEMB_TOOLCHAIN_PATH=${pkgs.gcc-arm-embedded}
+              export GNUARMEMB_TOOLCHAIN_PATH=${pkgs.gcc-arm-embedded-13}
               ${project-banner.packages.${system}.default}/bin/project-banner \
-              --owner "wallago" \
-              --logo " 󰖌 " \
-              --product "keyboard" \
-              --part "chocofi" \
-              --code "WL25-KB-CHOCOFI" \
-              --tips "west init -l config" \
-              --tips "west update" \
-              --tips "KB Left: west build -d build/left -b nice_nano@2 -s zmk/app -- -DSHIELD=chocofi_left" \
-              --tips "KB Right: west build -d build/right -b nice_nano@2 -s zmk/app -- -DSHIELD=chocofi_right"
+                --owner "wallago" \
+                --logo " 󰖌 " \
+                --product "keyboard" \
+                --part "chocofi" \
+                --code "WL25-KB-CHOCOFI" 
             '';
           };
         };
